265a9f6115eb30779991a6cb83d14d87b462126a
Vedanshi <vap4@njit.edu>
2022-03-30 22:26:43 -0400

    adding article pages

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -9,6 +9,10 @@ def test_request_main_menu_links(client):
     assert b'<a class="nav-link" href="/page2">Docker</a>' in response.data
     assert b'<a class="nav-link" href="/page3">Python/Flask</a>' in response.data
     assert b'<a class="nav-link" href="/page4">CI/CD</a>' in response.data
+    assert b'<a class="nav-link" href="/page5">Article1</a>' in response.data
+    assert b'<a class="nav-link" href="/page5">Article2</a>' in response.data
+    assert b'<a class="nav-link" href="/page5">Article3</a>' in response.data
+    assert b'<a class="nav-link" href="/page5">Article4</a>' in response.data
 
 def test_request_index(client):
     """This makes the index page"""
@@ -46,7 +50,27 @@ def test_request_page4(client):
     assert response.status_code == 200
     assert b"Project Setup" in response.data
 
-def test_request_page_not_found(client):
+def test_request_page5(client):
     """This makes the index page"""
     response = client.get("/page5")
-    assert response.status_code == 404
+    assert response.status_code == 200
+    assert b"Pylint" in response.data
+
+def test_request_page6(client):
+    """This makes the index page"""
+    response = client.get("/page6")
+    assert response.status_code == 200
+    assert b"AAA testing" in response.data
+
+def test_request_page7(client):
+    """This makes the index page"""
+    response = client.get("/page7")
+    assert response.status_code == 200
+    assert b"OOPs" in response.data
+
+def test_request_page8(client):
+    """This makes the index page"""
+    response = client.get("/page8")
+    assert response.status_code == 200
+    assert b"SOLID" in response.data
+
